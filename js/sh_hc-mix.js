ad89a4eba8d94941ea754419cd874bcf6a998bad
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2020-02-20 09:28:43 +0700

    Hỗ trợ hilight comment, property và giải thích

--- js/sh_hc-mix.js
+++ js/sh_hc-mix.js
@@ -4,12 +4,12 @@ if (! this.sh_languages) {
 sh_languages['hc-mix'] = [
   [ // Định nghĩa các thành phần trong ngôn ngữ lập trình như từ khoá hay hàm ...
     [
-      /^\s*([ABCD])\s*[\.:\)]\s*/gim,
+      /^\s*([A-Za-z])\s*[\.:\)]\s*/gim,
       'sh_hc-mix_answer',
       -1 // Mấy cái không tham gia vụ đóng mở ngoặc thì chỗ này là -1 donothing 
     ],
     [
-      /\s+([ABCD])\s*[\.:\)]\s*/gi,
+      /\s+([A-Za-z])\s*[\.:\)]\s*/gi,
       'sh_hc-mix_answer',
       -1
     ],
@@ -29,9 +29,19 @@ sh_languages['hc-mix'] = [
       -1
     ],
     [
-      /{/g,
+      /^\/\/.*?\n/gim,
       'sh_comment',
-      1
+      -1
+    ],
+    [
+      /^\[\<.+?\=.+?\>\]/gim,
+      'sh_function',
+      -1
+    ],
+    [
+      /^Giải thích: /gim,
+      'sh_string',
+      -1
     ],
   ],
   [ // Định nghĩa các cặp đóng mở như {...} (...) hay "..." '...'
@@ -55,9 +65,9 @@ sh_languages['hc-mix'].identify = function(){
       hcmix.html(hcmix.html().replace(regex, result));
   }
   $('.sh_hc-mix').each(function(index, el) {
-    replace(el, /\*_([a-d])_\*(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_correct sh_hc-mix_static'>$1</span><span class='sh_hc-mix_correct_sign sh_hc-mix_static'>$2</span>");
-    replace(el, /_([a-d])_(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_correct'>$1</span><span class='sh_hc-mix_correct_sign'>$2</span>");
-    replace(el, /\*([a-d])\*(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_static'>$1$2</span>");
+    replace(el, /\*_([A-Za-z])_\*(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_correct sh_hc-mix_static'>$1</span><span class='sh_hc-mix_correct_sign sh_hc-mix_static'>$2</span>");
+    replace(el, /_([A-Za-z])_(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_correct'>$1</span><span class='sh_hc-mix_correct_sign'>$2</span>");
+    replace(el, /\*([A-Za-z])\*(\s*(?:[\.:\)]|\s))/gi, "<span class='sh_hc-mix_static'>$1$2</span>");
     replace(el, /_(Nhóm\s+[0-9]+)(\s*[\.:\)]|\s)_/gi, "<span class='.sh_hc-mix_content_group'>$1</span><span class='sh_hc-mix_group'>$2</span>");
   });
 };
