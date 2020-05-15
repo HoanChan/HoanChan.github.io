4d2cb9c2db40b28603a97eec75b002ac49e5c019
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2020-05-15 22:58:13 +0700

    Nhập được file Tests lên

--- live/ide/js/editor.js
+++ live/ide/js/editor.js
@@ -19,6 +19,17 @@
   }
   //#endregion
   //#region Func 
+  function selectFolder() {
+    return new Promise(resolve => {
+      let input = $('<input type="file" name="files[]" id="files" multiple="" directory="" webkitdirectory="" mozdirectory="">').appendTo('body')[0];
+      input.onchange = _ => {
+        let files = Array.from(input.files);
+        input.remove();
+        resolve(files);
+      };
+      input.click();
+    });
+  }
   function selectFile(contentType, multiple) {
     return new Promise(resolve => {
       let input = document.createElement('input');
@@ -27,13 +38,21 @@
       input.accept = contentType;
       input.onchange = _ => {
         let files = Array.from(input.files);
+        input.remove();
         if (multiple)
           resolve(files);
         else
           resolve(files[0]);
-        input.remove();
       };
       input.click();
+    });
+  }
+
+  function ReadFile(file) {
+    return new Promise(resolve => {
+      let reader = new FileReader();
+      reader.onload = function () { resolve(reader.result); };
+      reader.readAsText(file);
     });
   }
 
@@ -119,22 +138,41 @@
     $(".ui.dropdown").dropdown();
     $(".ui.dropdown.site-links").dropdown({ action: "hide", on: "hover" });
     $(".ui.checkbox").checkbox();
+
     $(".message .close").on("click", function () {
       $(this).closest(".message").transition("fade");
     });
+
     $('#btnOpen').click(async function (e) {
       let file = await selectFile(".json", false);
-      let reader = new FileReader();
-      reader.onload = function () {
-        Data = JSON.parse(reader.result);
-        $('#btnAddProblem').parent().find(".field").remove();
-        for (let index = 0; index < Data.length; index++) {
-          let element = Data[index];
-          let pro = AddProblem(element.Name, index, false);
-          if (index == 0) pro.click();
-        }
-      };
-      reader.readAsText(file);
+      let fileData = await ReadFile(file);
+      Data = JSON.parse(fileData);
+      $('#btnAddProblem').parent().find(".field").remove();
+      for (let index = 0; index < Data.length; index++) {
+        let element = Data[index];
+        let pro = AddProblem(element.Name, index, false);
+        if (index == 0) pro.click();
+      }
+    });
+
+    $('#btnImport').click(async function (e) {
+      let files = await selectFolder();
+      files = files.filter(function (element, index, array) {
+        return element.name.endsWith('.inp') || element.name.endsWith('.out');
+      }).reverse(); // Đảo lại do file được lên danh sách ngược
+      if (files.length % 2 != 0) {
+        console.log('Files Count even');
+        console.log(files);
+        return;
+      }
+      let newIndex = Data[SelectedProblemIndex].Tests.length;
+      for (let index = 0; index < files.length; index += 2, newIndex++) {
+        let inputFile = await ReadFile(files[index + 1]);
+        let outputFile = await ReadFile(files[index]);
+        AddTest("Import Test " + (index / 2 + 1), newIndex, true);
+        Data[SelectedProblemIndex].Tests[newIndex].Input = inputFile.trim();
+        Data[SelectedProblemIndex].Tests[newIndex].Output = outputFile.trim();
+      }
     });
 
     $('#btnSave').click(function (e) {
@@ -179,10 +217,12 @@
         $('#TestList input:nth-child(' + index + ')').val(name);
       }
     });
+
     textEditor = $('#txtEdit').ckeditor(function () { }, options).editor;
     textEditor.on('change', function () {
       Data[SelectedProblemIndex].Content = GetContent();
     });
+
     window.MonacoResize = function () {
       textEditor.resize("100%", $("#editorContainer").height());
     };
